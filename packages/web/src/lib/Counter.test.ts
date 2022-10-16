fa281999c3a8ac2fcad4fd2a1c26e9442dd1595c
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2022-10-16 11:08:08 -0400

    UNIT TEST with playwright CRAZY

--- packages/web/src/lib/Counter.test.ts
+++ packages/web/src/lib/Counter.test.ts
@@ -1,25 +1,25 @@
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/svelte'
+import { fireEvent, render } from '@testing-library/svelte'
 import { describe, expect, test } from 'vitest'
 
 import Counter from './Counter.svelte'
 
 describe('Counter', () => {
   test('shows proper heading when rendered', () => {
-    const { container } = render(Counter, { count: 2 })
-    const button = screen.getByRole('button')
-    const counterText = container.querySelector('span')
+    const { container, getByRole } = render(Counter, { count: 34 })
+    const button = getByRole('button')
     expect(button).toBeDefined()
+    const counterText = container.querySelector('span')
     expect(counterText).toBeDefined()
-    expect(counterText.innerHTML).toEqual('count is 2')
+    expect(counterText.innerHTML).toEqual('count is 34')
   })
 
   // Note: This is as an async test as we are using `fireEvent`
   test('changes button text on click', async () => {
-    const { container } = render(Counter, { count: 2 })
+    const { container, getByTestId } = render(Counter, { count: 2 })
 
     const button: HTMLButtonElement = container.querySelector('button')
-    const value = screen.getByTestId('value')
+    const value = getByTestId('value')
 
     // Using await when firing events because we have to wait
     // for the next `tick` so that Svelte flushes all pending state changes.
