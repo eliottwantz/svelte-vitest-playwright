700d345800933cce1bf93a35c4c9a5f6d67835ab
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2022-10-15 22:53:25 -0400

    all tests pass

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000
   },
   test: {
-    include: ['src/**/*.test.ts'],
+    include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
     globals: true,
     environment: 'jsdom'
   }
